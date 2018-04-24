d356fc5a88ab30542a430f77a51a1e38dceb59aa
Muhammad <nhmishaq@gmail.com>
2018-04-24 13:45:18 -0400

    boxing

---
+++ boxingII/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace boxingII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Object> buddy = new List<Object>();
+            buddy.Add(7);
+            buddy.Add(28);
+            buddy.Add(-1);
+            buddy.Add(true);
+            buddy.Add("chair");
+            for (int i = 0; i<buddy.Count; i++){
+                if (buddy[i] is int){
+                    Console.WriteLine(buddy[i]);
+                }
+                if (buddy[i] is string){
+                    Console.WriteLine(buddy[i]);
+                }
+                if (buddy[i] is bool){
+                    Console.WriteLine(buddy[i]);
+                }
+            }
+            
+            
+        }
+    }
+}
